e0f230412f37789866a7ad2e9e23d606d7fa762f
Laura Thornhill <lcthornhill@gmail.com>
2019-02-10 21:58:52 +0000

    Moved Fts into individual files

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from .base import FunctionalTest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class LayoutAndStyling(FunctionalTest):
+    def test_layout_and_styling(self):
+        #Edith gotes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta = 10
+        )
+
