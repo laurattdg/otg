ad9ccc8bb9818f8976cd6b86ac8397235d36c6e8
Laura Thornhill <lcthornhill@gmail.com>
2019-02-10 09:30:26 +0000

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -60,10 +60,10 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
-
+        
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        
+
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         self.browser.get(self.live_server_url)
--- lists/views.py
+++ lists/views.py
@@ -1,19 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from lists.models import Item, List
+import pprint
 
 # Create your views here.
 def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'list.html', { 'items': items})
+def view_list(request, list_id):
+    list_=List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
+    return render(request, 'list.html', { 'list': list_ })
 
 
 def new_list(request):
     list_ = List.objects.create()
-
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect(f'/lists/{list_.id}/')
+
+
+def update_list(request, list_id):
+    list_=List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_id}/')
