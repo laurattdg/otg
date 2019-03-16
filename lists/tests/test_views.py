9badf24c0bde1abce898db526177c56ed0601413
Laura Thornhill <lcthornhill@gmail.com>
2019-03-16 20:07:11 +0000

    Refactor list view to handle new item POSTs

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -72,7 +72,7 @@ class NewItemTest(TestCase):
         correct_list = List.objects.create()
 
         self.client.post(
-            f'/lists/{correct_list.id}/add_item',
+            f'/lists/{correct_list.id}/',
             data={'item_text': 'A new item for an exisiting list'}
         )
 
@@ -81,14 +81,15 @@ class NewItemTest(TestCase):
         self.assertEqual(new_item.text, 'A new item for an exisiting list')
         self.assertEqual(new_item.list, correct_list)
 
-    def test_redirects_to_list_view(self):
+    def test_POST_redirects_to_list_view(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
         response = self.client.post(
-            f'/lists/{correct_list.id}/add_item',
+            f'/lists/{correct_list.id}/',
             data={'item_text': 'A new item for an exisiting list'}
         )
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
 
--- lists/views.py
+++ lists/views.py
@@ -3,14 +3,18 @@ from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
 from lists.models import Item, List
 
+from pprint import pprint
+
 # Create your views here.
 def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request, list_id):
+def view_list(request, list_id):    
     list_=List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect(f'/lists/{list_.id}/')
     return render(request, 'list.html', { 'list': list_ })
 
 
@@ -25,8 +29,3 @@ def new_list(request):
         return render(request, 'home.html', {'error': error})
     return redirect(f'/lists/{list_.id}/')
 
-
-def update_list(request, list_id):
-    list_=List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_id}/')
